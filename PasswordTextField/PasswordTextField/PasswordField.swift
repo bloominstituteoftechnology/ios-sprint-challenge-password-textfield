2aacc3c0914d2b159ec5dc5b9eea7af5ed2e0938
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-09-13 08:06:30 -0700

    Added titleContainerView var and titleLabel constraints

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -41,8 +41,15 @@ class PasswordField: UIControl {
     func setup() {
         // Lay out your subviews here
         
-        addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		// Title Label
+		addSubview(titleLabel)
+		titleLabel.translatesAutoresizingMaskIntoConstraints = false
+		titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+		titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 150).isActive = true
+		titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20).isActive = true
+		
+		titleLabel.text = "Enter Password:"
+		titleLabel.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
     }
     
     required init?(coder aDecoder: NSCoder) {
