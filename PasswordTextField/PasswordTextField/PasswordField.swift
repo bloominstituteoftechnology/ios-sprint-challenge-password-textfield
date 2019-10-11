150c2231fccf9e98f4d653f4c3e1d089d94b9913
Isvvc <lyons@tuta.io>
2019-10-11 11:48:57 -0600

    Fix view controller hierarchy to allow editing

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -62,7 +62,6 @@ class PasswordField: UIControl {
         textField.keyboardType = .asciiCapable
         textField.autocorrectionType = .no
         textField.isEnabled = true
-        textField.text = "1234567890"
         textField.placeholder = "Password"
         textField.isSecureTextEntry = true
         textField.delegate = self
@@ -164,10 +163,7 @@ class PasswordField: UIControl {
             strongView.heightAnchor.constraint(equalToConstant: colorViewSize.height)
         ])
         
-        password = textField.text!
         determineStrength()
-        showPasswordToggle()
-        sendActions(for: .valueChanged)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -217,43 +213,8 @@ extension PasswordField: UITextFieldDelegate {
         determineStrength()
         return true
     }
-    
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        // return NO to disallow editing.
-        print("TextField should begin editing method called")
-        return true
-    }
-
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // became first responder
-        print("TextField did begin editing method called")
-    }
-
-    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-        // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
-        print("TextField should snd editing method called")
-        return true
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
-        print("TextField did end editing method called")
-    }
-
-    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
-        // if implemented, called in place of textFieldDidEndEditing:
-        print("TextField did end editing with reason method called")
-    }
-
-    func textFieldShouldClear(_ textField: UITextField) -> Bool {
-        // called when clear button pressed. return NO to ignore (no notifications)
-        print("TextField should clear method called")
-        return true
-    }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // called when 'return' key pressed. return NO to ignore.
-        print("TextField should return method called")
         textField.resignFirstResponder()
         
         sendActions(for: .valueChanged)
