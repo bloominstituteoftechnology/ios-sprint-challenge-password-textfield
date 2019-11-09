9551702b4c252e0f049d60593e01dce4fd9bc662
Rwoltx <richardx1016@gmail.com>
2019-11-09 00:05:58 -0500

    Finished

--- PasswordTextField/PasswordTextField/ViewController.swift
+++ PasswordTextField/PasswordTextField/ViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+  
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
     
     
     @IBAction func textEntered(_ sender: PasswordField) {
