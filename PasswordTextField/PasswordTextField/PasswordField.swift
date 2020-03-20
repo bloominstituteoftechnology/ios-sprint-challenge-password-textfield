da41ec6327936f634bb6d38f0c9952e3820fcc37
Hector L <hectoremil21@hotmail.com>
2020-03-20 11:48:25 -0400

    Title label created and placed. Textfield created and placed, working on its functionality.

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PasswordField: UIControl {
     
-    // Public API - these properties are used to fetch the final password and strength values
+    // MARK: - Public API - these properties are used to fetch the final password and strength values
     private (set) var password: String = ""
     
     private let standardMargin: CGFloat = 8.0
@@ -24,11 +24,13 @@ class PasswordField: UIControl {
     private let textFieldBorderColor = UIColor(hue: 208/360.0, saturation: 80/100.0, brightness: 94/100.0, alpha: 1)
     private let bgColor = UIColor(hue: 0, saturation: 0, brightness: 97/100.0, alpha: 1)
     
-    // States of the password strength indicators
+    // MARK: - States of the password strength indicators
     private let unusedColor = UIColor(hue: 210/360.0, saturation: 5/100.0, brightness: 86/100.0, alpha: 1)
     private let weakColor = UIColor(hue: 0/360, saturation: 60/100.0, brightness: 90/100.0, alpha: 1)
     private let mediumColor = UIColor(hue: 39/360.0, saturation: 60/100.0, brightness: 90/100.0, alpha: 1)
     private let strongColor = UIColor(hue: 132/360.0, saturation: 60/100.0, brightness: 75/100.0, alpha: 1)
+    
+    // MARK: - UI Element Properties
     
     private var titleLabel: UILabel = UILabel()
     private var textField: UITextField = UITextField()
@@ -38,12 +40,48 @@ class PasswordField: UIControl {
     private var strongView: UIView = UIView()
     private var strengthDescriptionLabel: UILabel = UILabel()
     
+    
+    // MARK: - Methods
+    
     func setup() {
         // Lay out your subviews here
         
+        // MARK: - titleLabel
+        titleLabel.text = "ENTER PASSWORD"
+        titleLabel.font = labelFont
+        titleLabel.textColor = labelTextColor
         addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: standardMargin),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin)
+        ])
+        
+        // MARK: - Textfield
+        textField.borderStyle = .roundedRect
+        textField.layer.borderColor = textFieldBorderColor.cgColor
+        textField.layer.borderWidth = 2
+        textField.layer.cornerRadius = 5
+        textField.font = UIFont.systemFont(ofSize: 10)
+        textField.keyboardType = UIKeyboardType.default
+        textField.returnKeyType = UIReturnKeyType.default
+        textField.contentVerticalAlignment = .center
+        textField.textAlignment = .center
+        textField.textColor = .black
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.isUserInteractionEnabled = true
+        textField.delegate = self
+        addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: standardMargin),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: standardMargin),
+            textField.heightAnchor.constraint(equalToConstant: textFieldContainerHeight)
+        ])
     }
+    
+    
+    // MARK: - Initializer
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -51,12 +89,19 @@ class PasswordField: UIControl {
     }
 }
 
+
+// MARK: - Extension
+
 extension PasswordField: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let oldText = textField.text!
-        let stringRange = Range(range, in: oldText)!
-        let newText = oldText.replacingCharacters(in: stringRange, with: string)
-        // TODO: send new text to the determine strength method
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         return true
     }
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        let oldText = textField.text!
+//        let stringRange = Range(range, in: oldText)!
+//        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+//        // TODO: send new text to the determine strength method
+//        return true
+//    }
 }
