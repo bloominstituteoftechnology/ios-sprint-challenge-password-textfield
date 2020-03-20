21e22a89170056acf3386a6a993b6d96310c66f9
Hector L <hectoremil21@hotmail.com>
2020-03-20 15:35:55 -0400

    no change

--- PasswordTextField/PasswordTextField/ViewController.swift
+++ PasswordTextField/PasswordTextField/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     // in the lower left corner. Tap that button and choose a dictionary
     // to install (you can use the first one "American English"). Tap
     // the little cloud download button to install it. Then just stop the app
-    // and comment this method out again. This step only needs to run once.
+    // and comment this method out again. This step only     needs to run once.
     
 //    override func viewDidAppear(_ animated: Bool) {
 //        super.viewDidAppear(animated)
