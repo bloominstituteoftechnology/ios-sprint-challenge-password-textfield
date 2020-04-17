05d70c7c0f5216f4d41944aa9a1d6351587743ca
Hunter Oppel <hun.oppel@gmail.com>
2020-04-17 12:59:48 -0400

    views

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -12,6 +12,15 @@ class PasswordField: UIControl {
     
     // Public API - these properties are used to fetch the final password and strength values
     private (set) var password: String = ""
+    private (set) var strength: Strength = .weak
+    
+    enum Strength: String {
+        case weak = "Too weak"
+        case medium = "Could be stronger"
+        case strong = "Strong password"
+    }
+    
+    private var isPasswordHidden = true
     
     private let standardMargin: CGFloat = 8.0
     private let textFieldContainerHeight: CGFloat = 50.0
@@ -41,13 +50,125 @@ class PasswordField: UIControl {
     func setup() {
         // Lay out your subviews here
         
-        addSubview(titleLabel)
+        backgroundColor = bgColor
+        
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Enter Password"
+        titleLabel.textAlignment = .left
+        titleLabel.textColor = labelTextColor
+        titleLabel.font = labelFont
+        addSubview(titleLabel)
+        
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.textColor = labelTextColor
+        textField.font = labelFont
+        textField.textContentType = .newPassword
+        textField.placeholder = "password"
+        textField.heightAnchor.constraint(equalToConstant: textFieldContainerHeight).isActive = true
+        addSubview(textField)
+        
+        showHideButton.translatesAutoresizingMaskIntoConstraints = false
+        showHideButton.setImage(UIImage(named: "eyes-closed"), for: .normal)
+        showHideButton.addTarget(self, action: #selector(showHidePassword), for: .touchUpInside)
+        addSubview(showHideButton)
+        
+        weakView.translatesAutoresizingMaskIntoConstraints = false
+        weakView.backgroundColor = weakColor
+        weakView.frame = CGRect(x: 0, y: 0, width: 18, height: 3)
+        addSubview(weakView)
+        
+        mediumView.translatesAutoresizingMaskIntoConstraints = false
+        mediumView.backgroundColor = unusedColor
+        mediumView.frame = CGRect(x: 0, y: 0, width: 18, height: 3)
+        addSubview(mediumView)
+        
+        strongView.translatesAutoresizingMaskIntoConstraints = false
+        strongView.backgroundColor = unusedColor
+        mediumView.frame = CGRect(x: 0, y: 0, width: 18, height: 3)
+        addSubview(strongView)
+        
+        strengthDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        strengthDescriptionLabel.text = self.strength.rawValue
+        addSubview(strengthDescriptionLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 4),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 4),
+            
+            textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
+            textField.trailingAnchor.constraint(equalTo: showHideButton.leadingAnchor, constant: 4),
+            
+            showHideButton.topAnchor.constraint(equalTo: textField.topAnchor),
+            showHideButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 4),
+            showHideButton.heightAnchor.constraint(equalTo: showHideButton.widthAnchor),
+            
+            weakView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 4),
+            weakView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4),
+            
+            mediumView.topAnchor.constraint(equalTo: weakView.topAnchor),
+            mediumView.leadingAnchor.constraint(equalTo: weakView.trailingAnchor, constant: 4),
+            
+            strongView.topAnchor.constraint(equalTo: weakView.topAnchor),
+            strongView.leadingAnchor.constraint(equalTo: mediumView.trailingAnchor, constant: 4),
+            
+            strengthDescriptionLabel.topAnchor.constraint(equalTo: weakView.topAnchor),
+            strengthDescriptionLabel.leadingAnchor.constraint(equalTo: strongView.trailingAnchor, constant: 4),
+            strengthDescriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 4)
+        ])
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
+    }
+    
+//    // MARK: - Controls
+//
+//    override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+//        let touchPoint = touch.location(in: self)
+//        if textField.frame.contains(touchPoint) {
+//            textField.becomeFirstResponder()
+//        }
+//        sendActions(for: [.touchDown])
+//        return true
+//    }
+//
+//    override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+//        let touchPoint = touch.location(in: self)
+//        if bounds.contains(touchPoint) {
+//            sendActions(for: [.touchDragInside])
+//        } else {
+//            sendActions(for: [.touchDragOutside])
+//        }
+//        return true
+//    }
+//
+//    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+//        guard let touch = touch else { return }
+//
+//        let touchPoint = touch.location(in: self)
+//        if bounds.contains(touchPoint) {
+//            sendActions(for: [.touchUpInside])
+//        } else {
+//            sendActions(for: [.touchUpOutside])
+//        }
+//    }
+//
+//    override func cancelTracking(with event: UIEvent?) {
+//        sendActions(for: [.touchCancel])
+//    }
+    
+    @objc private func showHidePassword() {
+        isPasswordHidden = !isPasswordHidden
+        
+        switch isPasswordHidden {
+        case true:
+            showHideButton.setImage(UIImage(named: "eyes-closed"), for: .normal)
+        case false:
+            showHideButton.setImage(UIImage(named: "eyes-open"), for: .normal)
+        }
     }
 }
 
