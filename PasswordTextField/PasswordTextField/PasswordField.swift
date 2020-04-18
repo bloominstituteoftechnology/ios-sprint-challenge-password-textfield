5b83dfaf9fc4e4a930b729f2a6f1d134509cc507
James McDougall <jameswiliammcdougall@gmail.com>
2020-04-18 10:25:36 -0700

    Created password border

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -13,15 +13,16 @@ class PasswordField: UIControl {
     // Public API - these properties are used to fetch the final password and strength values
     private (set) var password: String = ""
     
+    
     private let standardMargin: CGFloat = 8.0
-    private let textFieldContainerHeight: CGFloat = 50.0
+    private let passwordViewContainerHeight: CGFloat = 50.0
     private let textFieldMargin: CGFloat = 6.0
     private let colorViewSize: CGSize = CGSize(width: 60.0, height: 5.0)
     
     private let labelTextColor = UIColor(hue: 233.0/360.0, saturation: 16/100.0, brightness: 41/100.0, alpha: 1)
     private let labelFont = UIFont.systemFont(ofSize: 14.0, weight: .semibold)
     
-    private let textFieldBorderColor = UIColor(hue: 208/360.0, saturation: 80/100.0, brightness: 94/100.0, alpha: 1)
+    private let passwordViewBorderColor = UIColor(hue: 208/360.0, saturation: 80/100.0, brightness: 94/100.0, alpha: 1)
     private let bgColor = UIColor(hue: 0, saturation: 0, brightness: 97/100.0, alpha: 1)
     
     // States of the password strength indicators
@@ -30,28 +31,51 @@ class PasswordField: UIControl {
     private let mediumColor = UIColor(hue: 39/360.0, saturation: 60/100.0, brightness: 90/100.0, alpha: 1)
     private let strongColor = UIColor(hue: 132/360.0, saturation: 60/100.0, brightness: 75/100.0, alpha: 1)
     
-    private var titleLabel: UILabel = UILabel()
+    private var enterPasswordLabel: UILabel = UILabel()
     private var textField: UITextField = UITextField()
     private var showHideButton: UIButton = UIButton()
     private var weakView: UIView = UIView()
     private var mediumView: UIView = UIView()
     private var strongView: UIView = UIView()
     private var strengthDescriptionLabel: UILabel = UILabel()
+    private let passwordView: UIView = UIView()
     
     func setup() {
         // Lay out your subviews here
-        
-        addSubview(titleLabel)
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.text = "Enter Password"
-        titleLabel.font = labelFont
-        titleLabel.textColor = labelTextColor
-        titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: standardMargin)
-        titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin)
-        
-        
+        configureEnterPasswordLabel()
+        configurePasswordView()
         
     }
+    
+    func configureEnterPasswordLabel() {
+        addSubview(enterPasswordLabel)
+        enterPasswordLabel.translatesAutoresizingMaskIntoConstraints = false
+        enterPasswordLabel.text = "Enter Password"
+        enterPasswordLabel.font = labelFont
+        enterPasswordLabel.textColor = labelTextColor
+        
+        NSLayoutConstraint.activate([
+            enterPasswordLabel.topAnchor.constraint(equalTo: topAnchor, constant: standardMargin),
+            enterPasswordLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin)
+        ])
+    }
+    
+    func configurePasswordView() {
+        addSubview(passwordView)
+        passwordView.translatesAutoresizingMaskIntoConstraints = false
+        passwordView.layer.borderColor = passwordViewBorderColor.cgColor
+        passwordView.layer.borderWidth = 1
+        passwordView.layer.cornerRadius = 10
+        
+        NSLayoutConstraint.activate([
+            passwordView.topAnchor.constraint(equalTo: enterPasswordLabel.bottomAnchor, constant: standardMargin),
+            passwordView.leadingAnchor.constraint(equalTo: enterPasswordLabel.leadingAnchor),
+            trailingAnchor.constraint(equalTo: passwordView.trailingAnchor, constant: standardMargin),
+            passwordView.heightAnchor.constraint(equalToConstant: passwordViewContainerHeight)
+        ])
+        
+    }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
