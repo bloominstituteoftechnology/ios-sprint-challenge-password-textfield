810cfa584c6a7e6b62c591b7dc0ee3396aaefccc
Dimitri Bouniol <dimitri.bouniol@lambda.com>
2020-04-19 23:52:41 -0400

    got a lot of work done, animations are next

--- PasswordTextField/PasswordTextField/PasswordField.swift
+++ PasswordTextField/PasswordTextField/PasswordField.swift
@@ -39,19 +39,108 @@ class PasswordField: UIControl {
     private var strengthDescriptionLabel: UILabel = UILabel()
     
     func setup() {
-        // Lay out your subviews here
         
+        self.backgroundColor = bgColor
+        
+        titleLabel.text = "ENTER PASSWORD"
+        titleLabel.textColor = labelTextColor
+        titleLabel.font = labelFont
         addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor)
+        ])
+        
+        textField.textContentType = .password
+        textField.isSecureTextEntry = false
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.layer.borderColor = textFieldBorderColor.cgColor
+        textField.layer.borderWidth = 1
+        addSubview(textField)
+        NSLayoutConstraint.activate([
+            textField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: textFieldMargin),
+            textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: textFieldMargin),
+            textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: textFieldMargin),
+            textField.heightAnchor.constraint(equalToConstant: textFieldContainerHeight),
+        ])
+        
+        showHideButton.translatesAutoresizingMaskIntoConstraints = false
+        showHideButton.setImage(UIImage(named: "eyes-open"), for: .normal)
+        showHideButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        addSubview(showHideButton)
+        NSLayoutConstraint.activate([
+            showHideButton.leadingAnchor.constraint(equalTo: textField.trailingAnchor, constant: -50.0),
+            showHideButton.topAnchor.constraint(equalTo: textField.topAnchor, constant: 10.0)
+        ])
+        
+        weakView.backgroundColor = weakColor
+        weakView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(weakView)
+        NSLayoutConstraint.activate([
+            weakView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
+            weakView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
+            weakView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: standardMargin),
+            weakView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: standardMargin),
+            weakView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -standardMargin)
+        ])
+        
+        mediumView.frame.size = colorViewSize
+        mediumView.backgroundColor = unusedColor
+        mediumView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(mediumView)
+        NSLayoutConstraint.activate([
+            mediumView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
+            mediumView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
+            mediumView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: standardMargin),
+            mediumView.leadingAnchor.constraint(equalTo: weakView.trailingAnchor, constant: standardMargin),
+            mediumView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -standardMargin)
+        ])
+        
+        strongView.frame.size = colorViewSize
+        strongView.backgroundColor = unusedColor
+        strongView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(strongView)
+        NSLayoutConstraint.activate([
+            strongView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
+            strongView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
+            strongView.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: standardMargin),
+            strongView.leadingAnchor.constraint(equalTo: mediumView.trailingAnchor, constant: standardMargin),
+            strongView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -standardMargin)
+        ])
+        
+        strengthDescriptionLabel.text = "Too weak"
+        strengthDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        strengthDescriptionLabel.textColor = labelTextColor
+        strengthDescriptionLabel.font = labelFont
+        addSubview(strengthDescriptionLabel)
+        NSLayoutConstraint.activate([
+            strengthDescriptionLabel.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: textFieldMargin),
+            strengthDescriptionLabel.leadingAnchor.constraint(equalTo: strongView.trailingAnchor, constant: standardMargin),
+            strengthDescriptionLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -3.0)
+        ])
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
+    
+    @objc func buttonTapped() {
+        if showHideButton.imageView?.image == UIImage(named: "eyes-open") {
+            textField.isSecureTextEntry = true
+            showHideButton.setImage(UIImage(named: "eyes-closed"), for: .normal)
+        } else {
+            textField.isSecureTextEntry = false
+            showHideButton.setImage(UIImage(named: "eyes-open"), for: .normal)
+        }
+    }
+    
 }
 
 extension PasswordField: UITextFieldDelegate {
+    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let oldText = textField.text!
         let stringRange = Range(range, in: oldText)!
@@ -59,4 +148,5 @@ extension PasswordField: UITextFieldDelegate {
         // TODO: send new text to the determine strength method
         return true
     }
-}
+    
+} //End of extension
