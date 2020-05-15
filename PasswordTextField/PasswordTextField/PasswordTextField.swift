4242a33c16ecd7d02e1e49d9ec697c3ccbe00bee
Joe VeVerka <joeveverka89@gmail.com>
2020-05-15 13:16:12 -0400

    minor tweaks

--- PasswordTextField/PasswordTextField/PasswordTextField.swift
+++ PasswordTextField/PasswordTextField/PasswordTextField.swift
@@ -6,17 +6,15 @@
 
 import UIKit
 
-
-
 enum PasswordStrength: Int {
     case weak
     case medium
     case strong
 }
-@IBDesignable class PasswordTextField: UIControl, UITextFieldDelegate {
+@IBDesignable class PasswordTextField: UIControl {
     
     // MARK: Private properties
-    
+    // Public API - these properties are used to fetch the final password and strength values
     private (set) var password: String = ""
     
     var strengthOfPassword: PasswordStrength {
@@ -30,11 +28,11 @@ enum PasswordStrength: Int {
         }
     }
     // Margin and size properties
-    private let margin: CGFloat = 8.0
-    private let tfContainerHeight: CGFloat = 50.0
-    private let colorViewSize: CGSize = CGSize(width: 60, height: 6.0)
-    private let textFieldMargins: CGFloat = 6.0
+    private let standardMargin: CGFloat = 8.0
+    private let textFieldContainerHeight: CGFloat = 50.0
+    private let textFieldMargin: CGFloat = 6.0
     private let cornerRadius: CGFloat = 5.0
+    private let colorViewSize: CGSize = CGSize(width: 60, height: 5.0)
     
     // Text field properties
     private let textFieldBorderWidth: CGFloat = 3.0
@@ -42,8 +40,9 @@ enum PasswordStrength: Int {
     private let tfBGColor = UIColor(hue: 0, saturation: 0, brightness: 97/100.0, alpha: 1)
     
     // Label properties
-    private let labelFontSize = UIFont.systemFont(ofSize: 12.0, weight: .semibold)
-    private let textColor = UIColor(hue: 233.0/360.0, saturation: 16/100.0, brightness: 41/100.0, alpha: 1)
+    private let labelTextColor = UIColor(hue: 233.0/360.0, saturation: 16/100.0, brightness: 41/100.0, alpha: 1)
+    private let labelFont = UIFont.systemFont(ofSize: 12.0, weight: .semibold)
+    
     
     // Creating view obbjects
     private var titleLabel: UILabel = UILabel()
@@ -53,20 +52,20 @@ enum PasswordStrength: Int {
     private var weakView: UIView = UIView()
     private var mediumView: UIView = UIView()
     private var strongView: UIView = UIView()
-    private var strengthDetailLabel: UILabel = UILabel()
+    private var strengthDescriptionLabel: UILabel = UILabel()
     
     // Password strength indicator color properties
-    private let noPassInTextField: UIColor = .lightGray
-    private let weakColor: UIColor = .red
-    private let mediumColor: UIColor = .yellow
-    private let strongColor: UIColor = .green
+    private let unusedColor = UIColor(hue: 210/360.0, saturation: 5/100.0, brightness: 86/100.0, alpha: 1)
+    private let weakColor = UIColor(hue: 0/360, saturation: 60/100.0, brightness: 90/100.0, alpha: 1)
+    private let mediumColor = UIColor(hue: 39/360.0, saturation: 60/100.0, brightness: 90/100.0, alpha: 1)
+    private let strongColor = UIColor(hue: 132/360.0, saturation: 60/100.0, brightness: 75/100.0, alpha: 1)
     
     // MARK: - private & internal helper funcs
     
     internal func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        strengthDetailLabel.text = "Too Weak"
-        mediumView.backgroundColor = noPassInTextField
-        strongView.backgroundColor = noPassInTextField
+        strengthDescriptionLabel.text = "Too Weak"
+        mediumView.backgroundColor = unusedColor
+        strongView.backgroundColor = unusedColor
         sendActions(for: .valueChanged)
         textField.resignFirstResponder()
         return true
@@ -79,36 +78,6 @@ enum PasswordStrength: Int {
             view.transform = .identity
         }
     }
-    // Fix it suggested change from private to internal func???
-    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let oldText = textField.text!
-        let stringRange = Range(range, in: oldText)!
-        let newText = oldText.replacingCharacters(in: stringRange, with: string)
-        password = newText
-        if newText != "" {
-            
-            switch strengthOfPassword {
-            case .weak:
-                strengthDetailLabel.text = "Too Weak"
-                mediumView.backgroundColor = noPassInTextField
-                strongView.backgroundColor = noPassInTextField
-            case .medium:
-                strengthDetailLabel.text = "Could Be Stronger"
-                if mediumView.backgroundColor == noPassInTextField {
-                    animate(view: mediumView)
-                    mediumView.backgroundColor = mediumColor
-                }
-                strongView.backgroundColor = noPassInTextField
-            case .strong:
-                strengthDetailLabel.text = "Strong Password"
-                if strongView.backgroundColor == noPassInTextField {
-                    animate(view: strongView)
-                    strongView.backgroundColor = strongColor
-                }
-            }
-        }
-        return true
-    }
     
     @objc private func eyeBalls() {
         if textField.isSecureTextEntry {
@@ -119,18 +88,19 @@ enum PasswordStrength: Int {
             showHideButton.setImage(UIImage(named: "eyes-closed"), for: .normal)
         }
     }
+    
     // MARK: - Stretch Goal
-    func isWordInDict() -> Bool {
+    func checkDictionary() -> Bool {
         if UIReferenceLibraryViewController.dictionaryHasDefinition(forTerm: password) {
             return true
         }
         return false
     }
     
-    
     // MARK: View Configuration
     
-    private func viewConfig() {
+    private func setup() {
+        isUserInteractionEnabled = false
         backgroundColor = UIColor(hue: 0, saturation: 0, brightness: 97/100.0, alpha: 1)
         
         // Text field config
@@ -142,18 +112,19 @@ enum PasswordStrength: Int {
         textField.becomeFirstResponder()
         textField.delegate = self
         textField.isSecureTextEntry = true
-        textField.setLeftPaddingPoints(margin)
+        textField.setLeftPaddingPoints(textFieldMargin)
         
         // Text field container
         textFieldContainer.translatesAutoresizingMaskIntoConstraints = false
+        textFieldContainer.addSubview(textField)
         addSubview(textFieldContainer)
         
         // Label Config
         addSubview(titleLabel)
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.text = "Enter Password"
-        titleLabel.textColor = textColor
-        titleLabel.font = labelFontSize
+        titleLabel.textColor = labelTextColor
+        titleLabel.font = labelFont
         
         // Strength view config
         weakView.translatesAutoresizingMaskIntoConstraints = false
@@ -163,20 +134,20 @@ enum PasswordStrength: Int {
         
         mediumView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(mediumView)
-        mediumView.backgroundColor = noPassInTextField
+        mediumView.backgroundColor = unusedColor
         mediumView.layer.cornerRadius = cornerRadius
         
         strongView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(strongView)
-        strongView.backgroundColor = noPassInTextField
+        strongView.backgroundColor = unusedColor
         strongView.layer.cornerRadius = cornerRadius
         
         // Strength detail label
-        strengthDetailLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(strengthDetailLabel)
-        strengthDetailLabel.text = "Too weak"
-        strengthDetailLabel.textColor = textColor
-        strengthDetailLabel.font = labelFontSize
+        strengthDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(strengthDescriptionLabel)
+        strengthDescriptionLabel.text = "Too weak"
+        strengthDescriptionLabel.textColor = labelTextColor
+        strengthDescriptionLabel.font = labelFont
         
         // Eyeballs
         showHideButton.translatesAutoresizingMaskIntoConstraints = false
@@ -187,52 +158,52 @@ enum PasswordStrength: Int {
         // MARK: Constraint Activation
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: margin),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: margin),
-            titleLabel.trailingAnchor.constraint(equalToSystemSpacingAfter: trailingAnchor, multiplier: -margin),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: standardMargin),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: standardMargin),
+            titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -standardMargin),
             
-            textFieldContainer.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: margin),
+            textFieldContainer.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: standardMargin),
             textFieldContainer.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
             textFieldContainer.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            textFieldContainer.heightAnchor.constraint(equalToConstant: tfContainerHeight),
+            textFieldContainer.heightAnchor.constraint(equalToConstant: textFieldContainerHeight),
             
-            textField.topAnchor.constraint(equalTo: textFieldContainer.topAnchor, constant: margin),
+            textField.topAnchor.constraint(equalTo: textFieldContainer.topAnchor, constant: standardMargin),
             textField.leadingAnchor.constraint(equalTo: textFieldContainer.leadingAnchor),
             textField.trailingAnchor.constraint(equalTo: textFieldContainer.trailingAnchor),
-            textField.bottomAnchor.constraint(equalTo: textFieldContainer.bottomAnchor, constant: -margin),
+            textField.bottomAnchor.constraint(equalTo: textFieldContainer.bottomAnchor, constant: -standardMargin),
             
-            weakView.topAnchor.constraint(equalTo: textFieldContainer.bottomAnchor, constant: margin),
+            weakView.topAnchor.constraint(equalTo: textFieldContainer.bottomAnchor, constant: standardMargin),
             weakView.leadingAnchor.constraint(equalTo: textFieldContainer.leadingAnchor),
             weakView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
             weakView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
             
             mediumView.topAnchor.constraint(equalTo: weakView.topAnchor),
-            mediumView.leadingAnchor.constraint(equalTo: weakView.trailingAnchor, constant: margin),
+            mediumView.leadingAnchor.constraint(equalTo: weakView.trailingAnchor, constant: standardMargin),
             mediumView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
             mediumView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
             
             strongView.topAnchor.constraint(equalTo: weakView.topAnchor),
-            strongView.leadingAnchor.constraint(equalTo: mediumView.trailingAnchor, constant: margin),
+            strongView.leadingAnchor.constraint(equalTo: mediumView.trailingAnchor, constant: standardMargin),
             strongView.widthAnchor.constraint(equalToConstant: colorViewSize.width),
             strongView.heightAnchor.constraint(equalToConstant: colorViewSize.height),
             
-            strengthDetailLabel.centerYAnchor.constraint(equalTo: strongView.centerYAnchor),
-            strengthDetailLabel.leadingAnchor.constraint(equalTo: strongView.trailingAnchor, constant: margin),
+            strengthDescriptionLabel.centerYAnchor.constraint(equalTo: strongView.centerYAnchor),
+            strengthDescriptionLabel.leadingAnchor.constraint(equalTo: strongView.trailingAnchor, constant: standardMargin),
             
             showHideButton.topAnchor.constraint(equalTo: textField.topAnchor),
-            showHideButton.trailingAnchor.constraint(equalTo: textField.trailingAnchor, constant: -margin),
+            showHideButton.trailingAnchor.constraint(equalTo: textField.trailingAnchor, constant: -standardMargin),
             showHideButton.bottomAnchor.constraint(equalTo: textField.bottomAnchor)
         ])
     }
     // MARK: - View LifeCyle
     required override init(frame: CGRect) {
         super.init(frame: frame)
-        viewConfig()
+        setup()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        viewConfig()
+        setup()
     }
 }
 // MARK: - View extension for set margins
@@ -241,6 +212,37 @@ extension UITextField {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.leftView = paddingView
         self.leftViewMode = .always
+    }
+}
+extension PasswordTextField : UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        password = newText
+        if newText != "" {
+            
+            switch strengthOfPassword {
+            case .weak:
+                strengthDescriptionLabel.text = "Too Weak"
+                mediumView.backgroundColor = unusedColor
+                strongView.backgroundColor = unusedColor
+            case .medium:
+                strengthDescriptionLabel.text = "Could Be Stronger"
+                if mediumView.backgroundColor == unusedColor {
+                    animate(view: mediumView)
+                    mediumView.backgroundColor = mediumColor
+                }
+                strongView.backgroundColor = unusedColor
+            case .strong:
+                strengthDescriptionLabel.text = "Strong Password"
+                if strongView.backgroundColor == unusedColor {
+                    animate(view: strongView)
+                    strongView.backgroundColor = strongColor
+                }
+            }
+        }
+        return true
     }
 }
 
